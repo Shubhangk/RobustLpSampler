4386e98c07ca45c1f5347ddfab7a820b33685106
Shubhangk <kulkar17@purdue.edu>
2018-07-11 11:53:44 -0700

    Delete DatasetGeneration.py

--- DatasetGeneration.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a = [i for i in range(20)]
-dataset = [2 * np.random.choice(a) for _ in range(100)]
-for tempvar in range(20):
-    dataset[tempvar] = 2
-np.random.shuffle(dataset)
-
-print(dataset)
